81d86f967c11ccbb432854e0869518b3fa11f140
sonia.bencomo <sonia.bencomo@vass.es>
2021-12-17 11:58:38 +0100

    Creación de PersonServiceTest. Funcionamiento de whenAddOnePersonWhitSameIDThenResultKo y whenAddOnePersonThenResultOk.

--- src/test/java/com/example/excercise/service/PersonServiceTest.java
+++ src/test/java/com/example/excercise/service/PersonServiceTest.java
@@ -1,13 +1,25 @@
 package com.example.excercise.service;
 
 import com.example.excercise.dto.PersonDto;
+import com.example.excercise.dto.ResponseDto;
+import com.example.excercise.entity.Person;
 import com.example.excercise.repository.PersonRepository;
-import org.junit.Before;
+import net.bytebuddy.dynamic.DynamicType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 public class PersonServiceTest {
@@ -18,26 +30,70 @@ public class PersonServiceTest {
     @InjectMocks
     PersonService personService;
 
-    @Before
+    private PersonDto personDto;
+
+    @BeforeEach
     public void init() {
         MockitoAnnotations.initMocks(this);
+
+        personDto = new PersonDto();
+        personDto.setId(1);
+        personDto.setPhone("651597587");
+        personDto.setEmail("sonia.bencomo@vass.es");
+        personDto.setPassword("soniabencomo");
+        personDto.setUserStatus(1);
+        personDto.setLastName("Bencomo Jorge");
+        personDto.setFirstName("Sonia");
+        personDto.setUserName("sonia.bencomo");
     }
 
     @Test
-    public void whenAddOnePersonThenResultOk(){
+    public void whenAddOnePersonThenResultOk() {
+        Mockito.when(personRepository.findById(1)).thenReturn(Optional.empty());
+        // when
+        ResponseDto responseDto = personService.addOnePerson(personDto);
+        // then
+        assertEquals("OK",responseDto.getCode());
+    }
 
-         PersonDto personDto = new PersonDto();
-         Integer id = 1;
-         String userName = "a";
-         String firstName = "b";
-         String lastName = "c";
-         String email = "d";
-         String password = "e";
-         String phone = "f";
-         Integer userStatus = 2;
+    @Test
+    public void whenAddOnePersonWhitSameIDThenResultKo(){
+        Mockito.when(personRepository.findById(1)).thenReturn(Optional.of(new Person()));
+        // when
+        ResponseDto responseDto = personService.addOnePerson(personDto);
+        // then
+        assertEquals("KO",responseDto.getCode());
+    }
 
-         //personService.addOnePerson(personDto);
+    @Test
+    public void whenDeleteOnePersonThenResultOk(){
 
+        personService.deleteOnePerson(personDto.getId());
+    }
+
+    @Test
+    public void whenDeleteOnePersonNotFoundThenResultKo(){
+
+    }
+
+    @Test
+    public void whenGetPersonByIdThenResultOk(){
+
+    }
+
+    @Test
+    public void whenGetPersonByIdNotFoundThenResultKo(){
+
+    }
+
+    @Test
+    public void whenGetAllPeopleThenResultOk(){
+        //when(personRepository.findAll()).thenReturn(Arrays.asList(personDto));
+        assertNotNull(personService.getAllPeople());
+    }
+
+    @Test
+    public void whenGetAllPeopleIsEmptyThenResultKo(){
 
     }
 }
